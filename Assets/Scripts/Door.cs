bfa16e814ee06fb385deb4d68116a66bffd3f5ae
SoffeSof <Sofie.d.andersen@hotmail.dk>
2024-11-28 02:21:12 +0100

    doorshaking effect

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    //Scripts
     public Interactable interactable;
+
+    //Door State Variables
     public bool isOpen = false;
-    private bool isLocked = false;
+    public bool isLocked = false;
     private bool isAnimating = false;
+
+    //Door Movement Variables
     public float openSpeed = 0.5f;
     public float closeSpeed = 1.0f;
     public float openAngle = 90.0f;
@@ -15,15 +20,34 @@ public class Door : MonoBehaviour
 
     private float currentAngle = 0.0f;
 
+    //Display messages
     public string openDisplayMessage = "Open Door";
     public string closeDisplayMessage = "Close Door";
+    public string lockedDisplayMessage = "Locked";
 
+    //Sound variables
     public AudioSource openSound; // Sound to play when the door opens
     public AudioSource closeSound; // Sound to play when the door closes
+    public AudioSource lockedSound; // Sound to play when the door closes
+
+    //Shaking door variables
+    public float shakeAmount = 0.05f; // Amount of shake
+    public float shakeDuration = 0.5f; // Duration of shake
+
+    private Vector3 originalPosition;
 
     public void Awake()
     {
         interactable = GetComponent<Interactable>();
+        originalPosition = transform.position; // Store the original position
+        if (isLocked)
+        {
+            interactable.displayMessage = lockedDisplayMessage;
+        }
+        else
+        {
+            interactable.displayMessage = openDisplayMessage;
+        }
     }
 
     public void Open()
@@ -52,9 +76,13 @@ public class Door : MonoBehaviour
                 Open();
                 interactable.displayMessage = closeDisplayMessage;
             }
+            isOpen = !isOpen;
         }
-
-        isOpen = !isOpen;
+        else
+        {
+            interactable.displayMessage = lockedDisplayMessage;
+            StartCoroutine(ShakeDoor());
+        }
     }
 
     private IEnumerator RotateDoor(float targetAngle)
@@ -73,6 +101,24 @@ public class Door : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(0f, targetAngle, 0f); // Snap to final angle
         isAnimating = false;
+    }
+
+    private IEnumerator ShakeDoor()
+    {
+        float elapsedTime = 0f;
+        lockedSound.Play();
+
+        while (elapsedTime < shakeDuration)
+        {
+            float shakeZ = Random.Range(-shakeAmount, shakeAmount);
+
+            transform.position = originalPosition + new Vector3(0f, 0f, shakeZ);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = originalPosition; // Reset position after shaking
     }
 }
 
